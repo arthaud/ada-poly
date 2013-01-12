9a078990dec9c7299f024583c7eb448ce4fbec9e
Maxima <maxime.arthaud@gmail.com>
2013-01-12 22:01:53 +0100

    Ajout de p_polynome.ads

---
+++ p_polynome.ads
@@ -0,0 +1,40 @@
+with p_arbre_poly; use p_arbre_poly;
+
+------------------------
+-- Paquetage polynôme
+------------------------
+package p_polynome is
+
+  type polynome is private;
+
+  -- Fonction Encoder
+  -- Sémantique : Prend un polynôme sous forme de chaine de caractères, et retourne un polynôme
+  -- Paramètres : p : string (D)
+  -- Précondition : p vérifie les contraintes du sujet
+  -- Postcondition : le polynôme retourné est p
+  -- function Encoder(p : in string) return polynome;
+
+  -- Fonction Decoder
+  -- Sémantique : Prend un polynôme et retourne la chaine de caractères correspondante
+  -- Paramètres : p : polynome (D)
+  -- Précondition : /
+  -- Postcondition : le polynôme retourné est p
+  -- function Decoder(p : in polynome) return string;
+
+  -- Fonction Ajouter
+  -- Sémantique : Fait l'addition de deux polynomes
+  -- Paramètres : p1 : polynome (D)
+  --              p2 : polynome (D)
+  -- Précondition : /
+  -- Postcondition : la sortie vaut p1 + p2
+  function Ajouter(p1 : in polynome; p2 :  in polynome) return polynome;
+
+  private
+    type polynome is new arbre_poly;
+    -- Invariants respectés par le type polynome :
+    -- * les fils sont rangés dans l'ordre : de la plus petite puissance à la plus grande
+    -- * Pour un noeud, 
+    --   Si const = 0, var /= ' ' et le noeud a au moins un fils
+    --   Sinon const /= 0, var = ' ' et le noeud n'a aucun fils
+
+end p_polynome;
